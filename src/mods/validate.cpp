c7e6bbaf48c5a0aed9bd704a9e7a01018d44fb1c
eddy0777 <eddynorman132@gmail.com>
2026-08-11 13:58:42 -0500

    remove migrated code from validate

--- src/mods/validate.cpp
+++ src/mods/validate.cpp
@@ -11,16 +11,8 @@
 
 namespace validate {
 
-constexpr u8 TIME_BETWEEN_TAPE_BREAK_AND_GOAL_SUBMODE = 3;
-static u8 s_frames_until_goal_submode = 0;
-
-// true during postgoal, and true during game scenario return *if* we enter it after
-// successfully entering a goal
-static bool s_is_postgoal_extended = false;
-
 // calculate from did_ball_enter_goal for subtick timer
 static u32 s_framesave;
-
 // values for validation
 static bool s_entered_goal;
 static bool s_used_mods;
@@ -188,40 +180,6 @@ void find_framesave(mkb::Ball* ball, int* out_stage_goal_idx, int* out_itemgroup
     } while (true);
 }
 
-bool has_entered_goal() { return s_entered_goal; }
-
-// Remedy for goal submode checks being delayed from tape break
-bool is_postgoal_exact() {
-    return (s_frames_until_goal_submode != 0 && mode::is_gameplay_main(mkb::sub_mode)) ||
-           mode::is_postgoal(mkb::sub_mode);
-}
-
-bool is_postgoal_exact_with_game_return() {
-    return is_postgoal_exact() || mode::is_game_scenario_return(mkb::sub_mode);
-}
-
-// Includes the game scenario return frame *if* we entered the goal before entering it
-// For example, if we stage select mid gameplay, this will return false on the game
-// scenario return frame
-bool is_postgoal_extended() { return is_postgoal_exact() || s_is_postgoal_extended; }
-
-bool is_gameplay_exact() { return mode::is_gameplay(mkb::sub_mode) && !is_postgoal_exact(); }
-
-// We can use is_postgoal_extended() to get the following useful status functions during a story
-// mode run (these are used in storytimer.cpp and deathcounter.cpp)
-
-bool is_between_worlds() { return mode::is_between_worlds(is_postgoal_extended()); }
-
-bool is_run_complete() { return mode::is_run_complete(is_postgoal_extended()); }
-
-void update_extended_postgoal_status() {
-    s_mode_tick_tramp.dest();
-    // Run this after the game updates the submode
-    if (!is_postgoal_exact_with_game_return()) {
-        s_is_postgoal_extended = false;
-    }
-}
-
 void init() {
     patch::hook_function(
         s_goal_tramp, &mkb::did_ball_enter_goal,
@@ -233,17 +191,9 @@ void init() {
                 // determine framesave percentage
                 find_framesave(ball, out_stage_goal_idx, out_itemgroup_id, out_goal_flags);
                 s_entered_goal = result;
-                s_is_postgoal_extended = result;
-                s_frames_until_goal_submode = TIME_BETWEEN_TAPE_BREAK_AND_GOAL_SUBMODE;
-            }
-            // We don't need to check if we're paused because this function doesn't get run while
-            // paused
-            if (s_frames_until_goal_submode != 0) {
-                s_frames_until_goal_submode -= 1;
             }
             return result;
         });
-    patch::hook_function(s_mode_tick_tramp, mkb::mode_tick, update_extended_postgoal_status);
 }
 
 void tick() {
@@ -253,17 +203,9 @@ void tick() {
         s_has_paused = false;
         s_loaded_savestate = false;
     }
-
-    // Handle cases where we pause (and either leave the stage or retry) or load state immediately
-    // after breaking the tape
-    // Note: savest_ui's tick gets run before validate's tick, so if we load state right after
-    // breaking the tape, there won't be a frame where we're in gameplay and
-    // s_frames_until_goal_submode is nonzero
-    if (mode::is_stage_exit_init(mkb::sub_mode) || libsavest::state_loaded_this_frame() ||
-        mode::is_spin_in_init(mkb::sub_mode)) {
-        s_frames_until_goal_submode = 0;
-    }
 }
+
+bool has_entered_goal() { return s_entered_goal; }
 
 bool was_run_valid(bool mods_allowed) {
     return (!s_used_mods || mods_allowed) && !s_has_paused && !s_loaded_savestate && s_entered_goal;
--- src/mods/validate.h
+++ src/mods/validate.h
@@ -8,13 +8,7 @@ void validate_run();
 bool was_run_valid(bool mods_allowed);
 void disable_invalidating_settings();
 u32 get_framesave();
-
 bool has_entered_goal();
-bool is_postgoal_exact();
-bool is_postgoal_extended();
-bool is_gameplay_exact();
-bool is_between_worlds();
-bool is_run_complete();
 
 void init();
 void tick();
